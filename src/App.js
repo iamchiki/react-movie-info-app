d8bd15ad17d8bc99bb620e5d4ab0b519989ed169
iamchiki <nikhil.soni599@gmail.com>
2021-10-23 22:48:10 +0530

    feat: add context api to manage state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React from 'react';
 import HeaderComponent from './components/HeaderComponent';
 import MovieSection from './components/MovieSection';
+import MovieContext from './store/movie-context';
 
 function App() {
+  const movies = [
+    {
+      Title: 'Stree',
+      Year: '2018',
+      imdbID: 'tt8108202',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Stree',
+      Year: '2018',
+      imdbID: 'tt8108203',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Stree',
+      Year: '2018',
+      imdbID: 'tt8108204',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Stree',
+      Year: '2018',
+      imdbID: 'tt8108205',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Stree',
+      Year: '2018',
+      imdbID: 'tt8108206',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
+    },
+  ];
   return (
-    <React.Fragment>
+    <MovieContext.Provider
+      value={{
+        movieList: movies,
+      }}>
       <HeaderComponent></HeaderComponent>
       <MovieSection></MovieSection>
-    </React.Fragment>
+    </MovieContext.Provider>
   );
 }
 
--- src/components/MovieSection.js
+++ src/components/MovieSection.js
@@ -1,51 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import MovieContext from '../store/movie-context';
 import MovieCard from './MovieCard';
 
 const MovieSection = () => {
-  const moviesArr = [
-    {
-      Title: 'Stree',
-      Year: '2018',
-      imdbID: 'tt8108202',
-      Type: 'movie',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
-    },
-    {
-      Title: 'Stree',
-      Year: '2018',
-      imdbID: 'tt8108203',
-      Type: 'movie',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
-    },
-    {
-      Title: 'Stree',
-      Year: '2018',
-      imdbID: 'tt8108204',
-      Type: 'movie',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
-    },
-    {
-      Title: 'Stree',
-      Year: '2018',
-      imdbID: 'tt8108205',
-      Type: 'movie',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
-    },
-    {
-      Title: 'Stree',
-      Year: '2018',
-      imdbID: 'tt8108206',
-      Type: 'movie',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg',
-    },
-  ];
+  const movieCtx = useContext(MovieContext);
 
-  const movieCardList = moviesArr.map((movie) => {
+  const movieCardList = movieCtx.movieList.map((movie) => {
     return (
       <li key={movie.imdbID} className='col-auto'>
         <MovieCard movieItem={movie}></MovieCard>
