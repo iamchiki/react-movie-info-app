7d03965802bdaee7c2ef4c51cc29139c9aefeb03
iamchiki <nikhil.soni599@gmail.com>
2021-10-23 22:46:29 +0530

    feat: implement forward ref

--- src/components/UI/SearchForm.js
+++ src/components/UI/SearchForm.js
@@ -3,10 +3,16 @@ import ButtonComponent from './ButtonComponent';
 import Input from './Input';
 
 const SearchForm = () => {
+  const inputRef = React.createRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+  };
+
   return (
     <div>
-      <form className='form-inline my-2 my-lg-0'>
-        <Input></Input>
+      <form className='form-inline my-2 my-lg-0' onSubmit={submitHandler}>
+        <Input ref={inputRef}></Input>
         <ButtonComponent>Search</ButtonComponent>
       </form>
     </div>
