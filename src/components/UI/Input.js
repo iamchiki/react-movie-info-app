c9ad356ea28f43bcd54a716d9bd122f1f97b2700
iamchiki <nikhil.soni599@gmail.com>
2021-10-23 22:44:43 +0530

    feat: implement forward ref to movie input value

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const Input = () => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <input
       className='form-control mr-sm-2'
       type='search'
       placeholder='Search'
       aria-label='Search'
+      ref={ref}
     />
   );
-};
+});
 
 export default Input;
