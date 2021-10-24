5c4304f996aeb4e30476ff51013726c22aa4dba6
iamchiki <nikhil.soni599@gmail.com>
2021-10-24 23:18:02 +0530

    feat: add fetching movies based on query text

--- src/components/UI/SearchForm.js
+++ src/components/UI/SearchForm.js
@@ -16,11 +16,27 @@ const SearchForm = (props) => {
         'https://m.media-amazon.com/images/M/MV5BMWU4ZjNlNTQtOGE2MS00NDI0LWFlYjMtMmY3ZWVkMjJkNGRmXkEyXkFqcGdeQXVyNjE1OTQ0NjA@._V1_SX300.jpg',
     },
   ];
+
+  const fetchMovies = async (queryText) => {
+    try {
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=3b8cf2cb&s=${queryText}`
+      );
+      const movies = await response.json();
+      console.log(movies);
+      movieCtx.movieList = [...movies.Search];
+      movieCtx.diaplayMovies(movieCtx.movieList);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    movieCtx.movieList = [...arr];
-    movieCtx.diaplayMovies(movieCtx.movieList);
+    // movieCtx.movieList = [...arr];
+    // movieCtx.diaplayMovies(movieCtx.movieList);
     // props.onSearch(movieCtx.movieList);
+    fetchMovies(inputRef.current.value);
   };
 
   return (
