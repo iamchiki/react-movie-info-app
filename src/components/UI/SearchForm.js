f13562d16467de7b700ddd9a4c37a2fb166881e9
iamchiki <nikhil.soni599@gmail.com>
2021-10-27 19:24:55 +0530

    refactor: update fetching url

--- src/components/UI/SearchForm.js
+++ src/components/UI/SearchForm.js
@@ -3,39 +3,28 @@ import MovieContext from '../../store/movie-context';
 import ButtonComponent from './ButtonComponent';
 import Input from './Input';
 
+const API_KEY = '3b8cf2cb&s';
+
 const SearchForm = (props) => {
   const inputRef = React.createRef();
   const movieCtx = useContext(MovieContext);
-  const arr = [
-    {
-      Title: 'Uri: The Surgical Strike',
-      Year: '2019',
-      imdbID: 'tt8291224',
-      Type: 'movie',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMWU4ZjNlNTQtOGE2MS00NDI0LWFlYjMtMmY3ZWVkMjJkNGRmXkEyXkFqcGdeQXVyNjE1OTQ0NjA@._V1_SX300.jpg',
-    },
-  ];
 
   const fetchMovies = async (queryText) => {
     try {
       const response = await fetch(
-        `https://www.omdbapi.com/?apikey=3b8cf2cb&s=${queryText}`
+        `https://www.omdbapi.com/?apikey=${API_KEY}=${queryText}`
       );
       const movies = await response.json();
-      console.log(movies);
       movieCtx.movieList = [...movies.Search];
       movieCtx.diaplayMovies(movieCtx.movieList);
     } catch (error) {
       console.log(error);
+      console.log(error.message);
     }
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
-    // movieCtx.movieList = [...arr];
-    // movieCtx.diaplayMovies(movieCtx.movieList);
-    // props.onSearch(movieCtx.movieList);
     fetchMovies(inputRef.current.value);
   };
 
